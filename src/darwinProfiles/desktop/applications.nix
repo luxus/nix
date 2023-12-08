7b0fe64cc32721f709579598d4fa1fbf53c41433
luxus <luxuspur@gmail.com>
2023-12-08 23:31:41 +0100

    brew app changes

--- src/darwinProfiles/desktop/applications.nix
+++ src/darwinProfiles/desktop/applications.nix
@@ -11,7 +11,7 @@ with builtins // lib; {
   homebrew.onActivation.autoUpdate = true;
   homebrew.onActivation.upgrade = true;
   homebrew.onActivation.cleanup = "uninstall";
-
+  homebrew.brews = [ "turso" "gnu-sed" ];
   # Add taps.
   homebrew.taps = [
     "buo/cask-upgrade"
@@ -21,86 +21,45 @@ with builtins // lib; {
     "homebrew/command-not-found"
     "homebrew/core"
     "homebrew/services"
+    "tursodatabase/tap"
   ];
 
   # Add casks.
   homebrew.casks =
     pipe
     ''
-      1password
-      1password-cli
-      adobe-creative-cloud
-      adrive
-      alt-tab
-      amethyst
-      applite
-      arc
-      baidunetdisk
-      balenaetcher
-      bilibili
-      clashx
-      cloudflare-warp
-      cursor
-      cyberduck
-      discord
-      element
-      feishu
-      fig
-      firefox
-      github
-      google-chrome
-      google-drive
-      hiddenbar
-      iina
-      itsycal
-      jetbrains-toolbox
-      keepingyouawake
-      keka
-      kitty
-      logitech-options
-      maccy
-      macfuse
-      microsoft-auto-update
-      microsoft-office
-      microsoft-remote-desktop
-      miniforge
-      monitorcontrol
-      mos
-      motrix
-      mounty
-      neteasemusic
-      notion
-      obs
-      obsidian
-      onyx
-      orbstack
-      qfinder-pro
-      qq
-      qsync-client
-      qudedup-extract-tool
-      raycast
-      rectangle
-      sioyek
-      spotify
-      squirrel
-      stats
-      steam
-      surge4
-      tailscale
-      telegram
-      tencent-lemon
-      tencent-meeting
-      transmission
-      utm
-      virtualbox
-      visual-studio-code
-      warp
-      wechat
-      wezterm
-      xpra
-      xquartz
-      zed
-      zotero
+    steam
+    keycastr
+    aldente
+    espanso
+    superslicer
+    qlmarkdown
+    microsoft-teams
+    cloudflare-warp
+    podman-desktop
+    scroll-reverser
+    raycast
+    zoom
+    logseq
+    balenaetcher
+    1password-beta
+    plexamp
+    utm
+    obsidian
+    gitkraken
+    plex
+    microsoft-edge-dev
+    lunar
+    visual-studio-code-insiders
+    sf-symbols
+    wireshark
+    iina
+    tidal
+    wez/wezterm/wezterm-nightly
+    microsoft-remote-desktop
+    raycast
+    telegram
+    warp
     ''
     [
       (splitString "\n")
