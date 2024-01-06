3a4f1d67b7d3c28e78fc3936f827e8a46e30b646
luxus <luxuspur@gmail.com>
2024-01-06 20:05:23 +0100

    comment out some stuff from upstream

--- src/homeProfiles/shell/ssh.nix
+++ src/homeProfiles/shell/ssh.nix
@@ -12,10 +12,10 @@ with builtins // lib; {
     sshping
   ];
 
-  home.file.".ssh/config".text = ''
-    Include config.d/home
-    Include config.d/lab
-  '';
-  home.file.".ssh/config.d/home".source = globals.root + /static/configs/ssh/home;
-  home.file.".ssh/config.d/lab".source = globals.root + /static/configs/ssh/lab;
+  # home.file.".ssh/config".text = ''
+  #   Include config.d/home
+  #   Include config.d/lab
+  # '';
+  # home.file.".ssh/config.d/home".source = globals.root + /static/configs/ssh/home;
+  # home.file.".ssh/config.d/lab".source = globals.root + /static/configs/ssh/lab;
 }
